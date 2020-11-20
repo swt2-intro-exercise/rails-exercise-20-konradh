c950490f0b07796f7f01ce3dbab09dba6f8e44ab
Konrad <>
2020-11-20 17:37:22 +0100

    Failing test for #2

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,8 +1,24 @@
 require 'rails_helper'
 
 describe "New author page", type: :feature do
-  
+
   it "should render withour error" do
     visit new_author_path
   end
+
+  describe "input fields" do
+    specify "first name" do
+      visit new_author_path
+      expect(page).to have_field('author[first_name]')
+    end
+    specify "last name" do
+      visit new_author_path
+      expect(page).to have_field('author[last_name]')
+    end
+    specify "homepage" do
+      visit new_author_path
+      expect(page).to have_field('author[homepage]')
+    end
+  end
+
 end
