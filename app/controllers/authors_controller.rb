1094de77e42bc37f2e947de07a0998ed2e73f1d1
Konrad <>
2020-11-22 12:35:54 +0100

    Implement new author saving #4

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,14 @@
 class AuthorsController < ApplicationController
   def new
   end
+
+  def create
+    Author.new(create_params).save
+    redirect_to root_path, notice: "Success!"
+  end
+
+  private
+  def create_params
+    params.require(:author).permit(:first_name, :last_name, :homepage)
+  end
 end
