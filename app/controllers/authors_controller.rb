d6b8dce3f8a3975a728fdf05cf4ecd443f2df722
Konrad <>
2020-11-22 14:37:39 +0100

    Implement update author #9

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,7 +4,7 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.new(create_params)
+    @author = Author.new(author_params)
     if @author.save
       redirect_to authors_path, notice: "Success!"
     else
@@ -20,8 +20,22 @@ class AuthorsController < ApplicationController
     @authors = Author.all
   end
 
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    @author = Author.find(params[:id])
+
+    if @author.update(author_params)
+      redirect_to @author
+    else
+      render edit_author_path
+    end
+  end
+
   private
-  def create_params
+  def author_params
     params.require(:author).permit(:first_name, :last_name, :homepage)
   end
 end
