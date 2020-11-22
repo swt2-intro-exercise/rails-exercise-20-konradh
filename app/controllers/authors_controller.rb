d83964314d44cd9d0646ae8e4d0afa8322fe81f3
Konrad <>
2020-11-22 12:55:47 +0100

    Add show author view and controller action #5

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,6 +7,10 @@ class AuthorsController < ApplicationController
     redirect_to root_path, notice: "Success!"
   end
 
+  def show
+
+  end
+
   private
   def create_params
     params.require(:author).permit(:first_name, :last_name, :homepage)
