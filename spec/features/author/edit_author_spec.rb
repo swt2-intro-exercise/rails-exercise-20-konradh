cdc24a9e3a037358473e5b1a30e114a5a1afcd54
Konrad <>
2020-11-22 14:37:09 +0100

    Fix tests for #9

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 describe "Edit author page", type: :feature do
+  before :each do
+    @alan = create(:author)
+  end
 
   it "renders without error" do
-    visit edit_author_path
+    visit edit_author_path(@alan)
   end
 
   it "updates an author's values" do
-    @alan = create(:author)
     visit edit_author_path(@alan)
     page.fill_in "author[last_name]", with: "Kay"
     page.fill_in "author[homepage]", with: "https://en.wikipedia.org/wiki/Alan_Kay"
