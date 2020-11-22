233cfdf5e52977c036df39ab267270b4cb3a7303
Konrad <>
2020-11-22 13:06:07 +0100

    Add failing test for #5

--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -8,4 +8,11 @@ describe "Show author page", type: :feature do
   it "should render without error" do
     visit author_path(@alan)
   end
+
+  it "should display first name, last name and homepage" do
+    visit author_path(@alan)
+    expect(page).to have_text(@alan.first_name)
+    expect(page).to have_text(@alan.last_name)
+    expect(page).to have_text(@alan.homepage)
+  end
 end
