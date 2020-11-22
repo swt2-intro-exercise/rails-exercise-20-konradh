da546186ae4782b991ec314976cd6bdb0f61f412
Konrad <>
2020-11-22 12:52:37 +0100

    Refactor tests

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -2,11 +2,15 @@ require 'rails_helper'
 
 describe "New author page", type: :feature do
 
-  it "should render withour error" do
+  it "should render without error" do
     visit new_author_path
   end
 
   describe "new author form" do
+    before :all do
+      @alan = create(:author)
+    end
+
     it "should have a field for first name, last name and homepage of an author" do
       visit new_author_path
       expect(page).to have_field('author[first_name]')
@@ -15,13 +19,12 @@ describe "New author page", type: :feature do
     end
 
     it "should save the values on submit" do
-      author = create(:author)
       authors_count = Author.count
 
       visit new_author_path
-      page.fill_in "author[first_name]", with: author.first_name
-      page.fill_in "author[last_name]", with: author.last_name
-      page.fill_in "author[homepage]", with: author.homepage
+      page.fill_in "author[first_name]", with: @alan.first_name
+      page.fill_in "author[last_name]", with: @alan.last_name
+      page.fill_in "author[homepage]", with: @alan.homepage
       find("input[type=submit]").click
 
       expect(Author.count).to eq authors_count + 1
