9de4710e5247089270f004e61b823b0f9d9a34ed
Konrad <>
2020-11-22 12:35:09 +0100

    Add failing test for #4

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -6,11 +6,26 @@ describe "New author page", type: :feature do
     visit new_author_path
   end
 
-  it "should have a form field for first name, last name and homepage of an author" do
-    visit new_author_path
-    expect(page).to have_field('author[first_name]')
-    expect(page).to have_field('author[last_name]')
-    expect(page).to have_field('author[homepage]')
+  describe "new author form" do
+    it "should have a field for first name, last name and homepage of an author" do
+      visit new_author_path
+      expect(page).to have_field('author[first_name]')
+      expect(page).to have_field('author[last_name]')
+      expect(page).to have_field('author[homepage]')
+    end
+
+    it "should save the values on submit" do
+      author = create(:author)
+      authors_count = Author.count
+
+      visit new_author_path
+      page.fill_in "author[first_name]", with: author.first_name
+      page.fill_in "author[last_name]", with: author.last_name
+      page.fill_in "author[homepage]", with: author.homepage
+      find("input[type=submit]").click
+
+      expect(Author.count).to eq authors_count + 1
+    end
   end
 
 end
